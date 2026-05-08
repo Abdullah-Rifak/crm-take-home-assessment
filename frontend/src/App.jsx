3f7b484a846c139bfdb300abf5816da00c3c05fa
Abdullah1 <abdullahrifak522@gmail.com>
2026-05-08 09:41:53 +0530

    "typo fixed"

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import Leads from "./pages/leads";
-import ProtectedRoute from "./componenets/protectedRoute.jsx";
+import ProtectedRoute from "./components/protectedRoute.jsx";
 
 function App() {
   useEffect(() => {
